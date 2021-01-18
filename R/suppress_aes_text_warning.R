b043d2fcdf6c30bbfc54605f7f178b519de6282d
J. Allen Baron <jabaron.phd@gmail.com>
2021-01-18 15:24:44 -0500

    Suggest ggplot2 for suppress_aes_text_warning()

--- R/suppress_aes_text_warning.R
+++ R/suppress_aes_text_warning.R
@@ -5,14 +5,28 @@
 #' Unnecessary, if converting to plotly for use as hovertext with `ggplotly()`.
 #'
 #' @examples
-#' g <- ggplot(iris) +
-#'     geom_point(aes(x = Sepal.Length, y = Sepal.Width, text = Species))
+#' requireNamespace("ggplot2", quietly = TRUE)
+#'
+#' g <- ggplot2::ggplot(iris) +
+#'     ggplot2::geom_point(
+#'         ggplot2::aes(x = Sepal.Length, y = Sepal.Width, text = Species)
+#'     )
 #' g
 #'
 #' suppress_aes_text_warning(g)
 #'
 #' @export
 suppress_aes_text_warning <- function(ggplot2_expr) {
+
+    if (!requireNamespace("ggplot2", quietly = TRUE)) {
+        err_msg <- paste(
+            strwrap("This function is designed to work with the \"ggplot2\" package. Please install it before use."),
+            collapse = "\n"
+        )
+
+        stop(err_msg, call. = FALSE)
+    }
+
     withCallingHandlers(
         {ggplot2_expr},
         warning = function(w) {
